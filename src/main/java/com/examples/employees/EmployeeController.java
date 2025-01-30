293bbfe10df3967e5ed8b1a0ff2b2f24985aff0d
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-30 22:09:36 +0000

    Support HTTP POST Requests

--- src/main/java/com/examples/employees/EmployeeController.java
+++ src/main/java/com/examples/employees/EmployeeController.java
@@ -1,17 +1,28 @@
 package com.example.employees;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    private final EmployeeManager employeeManager = new EmployeeManager();
+    private final EmployeeManager employeeManager;
 
+    public EmployeeController(EmployeeManager employeeManager) {
+        this.employeeManager = employeeManager;
+    }
+
+    // GET request to fetch all employees
     @GetMapping
-    public List<Employee> getAllEmployees() {
+    public Employees getEmployees() {
         return employeeManager.getEmployees();
+    }
+
+    // POST request to add a new employee
+    @PostMapping
+    public ResponseEntity<String> addEmployee(@RequestBody Employee employee) {
+        employeeManager.addEmployee(employee);
+        return new ResponseEntity<>("Employee added successfully!", HttpStatus.CREATED);
     }
 }
