efaef8693789d9cad841c7f7d91450c8f8b58cea
Thato Mongwe <thatomongwe47@gmail.com>
2025-01-30 22:08:42 +0000

    initialize some employees.

--- src/main/java/com/examples/employees/EmployeeManager.java
+++ src/main/java/com/examples/employees/EmployeeManager.java
@@ -1,19 +1,22 @@
 package com.example.employees;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmployeeManager {
-    private List<Employee> employees = new ArrayList<>();
+    private Employees employees;
 
     public EmployeeManager() {
-        // Hardcoded Employee Data
-        employees.add(new Employee("E001", "John", "Doe", "john.doe@example.com", "Software Engineer"));
-        employees.add(new Employee("E002", "Jane", "Smith", "jane.smith@example.com", "Product Manager"));
-        employees.add(new Employee("E003", "Alice", "Brown", "alice.brown@example.com", "HR Manager"));
+        employees = new Employees();
+        employees.addEmployee(new Employee("1", "John", "Doe", "john.doe@example.com", "Software Engineer"));
+        employees.addEmployee(new Employee("2", "Jane", "Smith", "jane.smith@example.com", "Project Manager"));
     }
 
-    public List<Employee> getEmployees() {
+    public Employees getEmployees() {
         return employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.addEmployee(employee);
     }
 }
